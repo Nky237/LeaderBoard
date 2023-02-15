2f2d4773ad0d8a874c93cf5bc297e911b242dae6
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-02-15 15:36:11 +0100

    added the contact section

--- src/pages/help/Contact.jsx
+++ src/pages/help/Contact.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
-        CONTACT
+    <div className='con'>
+        <h1>CONTACT US</h1>
+        <form action="">
+            <input type="text" placeholder='Name' />
+            <input type="email" placeholder='Email' />
+            <textarea name="" id="" cols="30" rows="10" placeholder='Leave a message'>
+
+            </textarea>
+            <button>Submit</button>
+        </form>
     </div>
   )
 }
