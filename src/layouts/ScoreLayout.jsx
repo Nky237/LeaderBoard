14fdf3beb063e8f4e45606bb8c0f83156382a8ee
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-02-18 22:39:32 +0100

    added some dummy contents

--- src/layouts/ScoreLayout.jsx
+++ src/layouts/ScoreLayout.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+import Trophy from '../assets/trophy.jfif'
 
 const ScoreLayout = () => {
   return (
     <div className='scoreLayout'>
-        <h1>WELCOME TO LEADERBOARD</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing
-             elit. Perferendis dolor ut laboriosam qui, inventore animi eveniet ex illum 
-             tempore vero?</p>
+        <h1 style={{textAlign:'center', paddingBottom: '20px'}}>WELCOME TO LEADERBOARD</h1>
+          <section className='Home'>
+              <main>
+              <p>Leaderboards with points rankings can be very useful for both companies and sports clubs, as they're a practical way to make a visual summary of players' or workers' results. This format gives information on how the competition has gone and summarizes the final results. Also, it's useful to communicate what each individual has achieved, as motivation for everyone.</p>
+             <p>To see the details of each track, kindly click on the track button</p>
+                <p>To see more details, click on the <b>see more</b> on each students card</p>
+
+                </main>  
+                <div>
+                    <img src={Trophy} alt="trophy" />
+                </div>
+          </section>        
+          
           <nav>
           <nav className='scoro'>
                 <NavLink to='fronto'>FRONTEND</NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,43 @@
 import React from 'react'
+import Learn1 from '../assets/learn1.jfif'
+import Learn2 from '../assets/learn2.jfif'
 
 const Home = () => {
   return (
-    <div><h2>Welcome</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor assumenda, itaque 
+    <div>
+      <h1 style={{textAlign:'center', padding:'10px 0'}}>WELCOME TO LEARNABLE</h1>
+        <section className='Home'>
+         <div>
+            <img src={Learn1} alt="" />
+         </div>
+          <main>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor assumenda, itaque 
         nesciunt alias 
         voluptatum nobis blanditiis eos officiis aperiam earum eum vel quas odio optio, distinctio 
         ab sunt unde incidunt ipsum omnis amet magnam accusantium aut! Excepturi, cupiditate iusto!</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus veniam ducimus
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus veniam ducimus
          eligendi nihil, cumque ab eveniet modi architecto quidem, non odit saepe facere voluptas esse mollitia illo fuga exercitationem id dicta iusto eaque
           numquam quaerat ad! Fugit velit beatae laudantium.</p>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus veniam ducimus 
-        eligendi nihil, cumque ab eveniet modi architecto quidem, non odit saepe facere voluptas 
-        esse mollitia illo fuga exercitationem id dicta iusto eaque numquam quaerat ad! Fugit velit
-         beatae laudantium.</p>
+
+          </main>
+        </section>
+        
+        <section className='Home'>
+          <main>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolor assumenda, itaque 
+        nesciunt alias 
+        voluptatum nobis blanditiis eos officiis aperiam earum eum vel quas odio optio, distinctio 
+        ab sunt unde incidunt ipsum omnis amet magnam accusantium aut! Excepturi, cupiditate iusto!</p>
+         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Necessitatibus veniam ducimus
+         eligendi nihil, cumque ab eveniet modi architecto quidem, non odit saepe facere voluptas esse mollitia illo fuga exercitationem id dicta iusto eaque
+          numquam quaerat ad! Fugit velit beatae laudantium.</p>
+          </main>
+          <div>
+            <img src={Learn2} alt="" />
+         </div>
+        </section>
+
+
          </div>
   )
 }
